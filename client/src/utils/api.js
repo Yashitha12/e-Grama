1081ba80c54af7fd7a3fc50023e7d4063603636b
Yashitha Dissanayaka <it22145048@my.sliit.lk>
2025-05-09 13:48:10 +0530

    Delete client directory

--- client/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // The base URL of your backend
-});
-
-export default api;  // Default export of the api instance
