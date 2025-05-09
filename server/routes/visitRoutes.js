5b8761a47de8d62aaae8db983343ab887942022f
Yashitha Dissanayaka <it22145048@my.sliit.lk>
2025-05-09 13:48:22 +0530

    Delete server directory

--- server/routes/visitRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createVisit, getVisits } = require('../controllers/visitController');
-const router = express.Router();
-
-router.post('/', createVisit); // Create a new visit
-router.get('/:patientId', getVisits); // Get visits for a patient
-
-module.exports = router;
